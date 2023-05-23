08edf4634646843c817a2f04be9b4ec369f25d6b
Raymond Kipkorir <raykipkorir02@gmail.com>
2023-05-23 11:54:15 +0300

    integrate swagger ui

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/schema/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger"),
 
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
