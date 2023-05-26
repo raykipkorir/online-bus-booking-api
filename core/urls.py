33edec43fa4fb241d44d5d6a2a53f40765b85506
Raymond Kipkorir <raykipkorir02@gmail.com>
2023-05-26 12:07:22 +0300

    fix bug

--- core/urls.py
+++ core/urls.py
@@ -1,21 +1,34 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.shortcuts import render
 from django.urls import include, path
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from drf_spectacular.views import (SpectacularAPIView, SpectacularRedocView,
+                                   SpectacularSwaggerView)
 from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView)
 
+
+def home(request):
+    return render(request, "index.html")
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", home),
 
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    path("api/schema/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger"),
+    path("api/v1/", include([
+        path("schema/", SpectacularAPIView.as_view(), name="schema"),
+        path("schema/swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger"),
+        path("schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+        path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+        path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-    path('api/', include('djoser.urls')),
+        path("", include('djoser.urls')),
 
-    path("api/", include("users.urls")),
-    path("api/", include("inventory.urls")),
-    path("api/", include("booking.urls")),
-]
+        path("", include("users.urls")),
+        path("", include("inventory.urls")),
+        path("", include("booking.urls")),
+    ]))
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from .serializers import AdminUserSerializer
@@ -7,7 +7,7 @@ from .serializers import AdminUserSerializer
 User = get_user_model()
 
 class AdminUserViewSet(ModelViewSet):
-
+    """Admin"""
     serializer_class = AdminUserSerializer
-    permission_classes = [IsAdminUser]
+    permission_classes = [IsAdminUser, IsAuthenticated]
     queryset = User.objects.filter(is_superuser=True)
